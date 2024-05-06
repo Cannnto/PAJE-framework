f779b54cd066f8c02ce181bd82e39e979a19754f
Cannnto <gabrielcantotdg517@gmail.com>
2024-05-06 17:50:03 -0300

    gravity

--- scripts/Player.js
+++ scripts/Player.js
@@ -1,5 +1,5 @@
 class Player{
-    constructor(x,y,width,height,sprite,speed,type,radius)
+    constructor(x,y,width,height,sprite,speed,type,radius,gravity)
     {
         this.width = width;
         this.height = height;
@@ -7,16 +7,17 @@ class Player{
         this.sprite.src = sprite;
         this.speed = speed;
         this.radius = radius;
+        this.gravityAcel = 0;
+        (gravity != undefined ? this.gravity = gravity : this.gravity = function(){});
+       
         if(type.toLowerCase() == 'square'){
             this.hitbox = new Polygono([new Point(x,y),
                                         new Point(x+this.width, y),
                                         new Point(x+this.width, y+this.height),
                                         new Point(x, y+this.height)
-                                       ])
+                                       ]);
         }
-        if(type.toLowerCase() == 'circle'){
-            this.hitbox = new Circle(x,y,radius)
-        }
+        if(type.toLowerCase() == 'circle')  this.hitbox = new Circle(x,y,radius);
     }
 
     moveAxis(dx,dy)
@@ -24,22 +25,16 @@ class Player{
         for(var i = 0; i<this.hitbox.points.length; i++)this.hitbox.points[i].translate(dx,dy)
     }
 
-
-    moveAcel(){
-
-    }
-    moveGravity(){
-
-    }
-
-
-
     draw()
     {   
         this.hitbox.draw();
     }
-
     update()
     {   
+        this.gravity();
     }
+}
+function gravity(){
+    this.gravityAcel+=1;
+    for(var i = 0; i<this.hitbox.points.length; i++) this.hitbox.points[i].translate(0,this.gravityAcel);
 }
