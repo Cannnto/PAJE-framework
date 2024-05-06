51bbeccf0e50a70530313cd9892fb2210bb21afb
Cannnto <gabrielcantotdg517@gmail.com>
2024-05-06 17:15:51 -0300

    Adicionado circle and square

--- scripts/Player.js
+++ scripts/Player.js
@@ -1,31 +1,38 @@
 class Player{
-    constructor(x,y,width,height,sprite,teclas,speed)
+    constructor(x,y,width,height,sprite,speed,type,radius)
     {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.sprite = new Image()
-        this.sprite.src = sprite
-        this.speed = speed
-        this.hitbox = new Polygono([new Point(this.x,this.y),
-                                   new Point(this.x+this.width, this.y),
-                                   new Point(this.x+this.width, this.y+this.height),
-                                   new Point(this.x, this.y+this.height)
-                                  ])
-        this.teclas = new keybinding(teclas)
+        this.width = width;
+        this.height = height;
+        this.sprite = new Image();
+        this.sprite.src = sprite;
+        this.speed = speed;
+        this.radius = radius;
+        if(type.toLowerCase() == 'square'){
+            this.hitbox = new Polygono([new Point(x,y),
+                                        new Point(x+this.width, y),
+                                        new Point(x+this.width, y+this.height),
+                                        new Point(x, y+this.height)
+                                       ])
+        }
+        if(type.toLowerCase() == 'circle'){
+            this.hitbox = new Circle(x,y,radius)
+        }
     }
 
-    move(dx,dy)
+    moveAxis(dx,dy)
     {   
-        for(var i = 0; i<this.hitbox.points.length; i++)
-        {
-            this.hitbox.points[i].x += dx*this.speed
-            this.hitbox.points[i].y += dy*this.speed
-        }
-        this.x += dx*this.speed
-        this.y += dy*this.speed
+        for(var i = 0; i<this.hitbox.points.length; i++)this.hitbox.points[i].translate(dx,dy)
     }
+
+
+    moveAcel(){
+
+    }
+    moveGravity(){
+
+    }
+
+
 
     draw()
     {   
@@ -33,7 +40,6 @@ class Player{
     }
 
     update()
-    {   this.draw()
-        this.teclas.pressed();
+    {   
     }
 }
--- scripts/geometry.js
+++ scripts/geometry.js
@@ -5,12 +5,13 @@ class Point
         this.y = y
     }
 
-    Translate(dx,dy)
-    {   this.x += dx
+    translate(dx,dy)
+    {           
+        this.x += dx
         this.y += dy
     }
 
-    Rotate(center,angle)
+    rotate(center,angle)
     {   angle = Math.PI / 180;
         let x = Math.cos(angle) * center.x - Math.sin(angle) * center.y;
         let y = Math.sin(angle) * center.x + Math.cos(angle) * center.y;
@@ -21,19 +22,21 @@ class Point
 
 class Polygono
 {
-    constructor(Points)
+    constructor(Points,radius)
     {   this.points = Points
+        this.radius = radius
     }
 
     draw()
-    {   context.strokeStyle = "black";
-        context.beginPath();
-        context.moveTo(this.points[0].x, this.points[0].y);
-        for (let point = 1; point < this.points.length; point++) {
-            context.lineTo(this.points[point].x, this.points[point].y)
-        }
-        context.closePath();
-        context.stroke();
+    {   
+            context.strokeStyle = "white";
+            context.beginPath();
+            context.moveTo(this.points[0].x, this.points[0].y);
+            for (let point = 1; point < this.points.length; point++) {
+                context.lineTo(this.points[point].x, this.points[point].y)
+            }
+            context.closePath();
+            context.stroke();
     }
 
     collide()
@@ -41,10 +44,16 @@ class Polygono
 
     }
 }
-
-/*class circle
-{   constructor()
-    {
-
+class Circle{
+    constructor(x,y,radius){
+        this.points = [new Point(x,y)]
+        this.radius = radius;
     }
-}*/
+    draw(){
+        context.strokeStyle = "white";
+        context.beginPath();
+        context.arc(this.points[0].x+this.radius, this.points[0].y+this.radius, this.radius, 0, Math.PI*2, true);
+        context.closePath();
+        context.stroke();
+    }
+}
