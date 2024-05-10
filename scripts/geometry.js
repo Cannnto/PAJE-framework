b88f41552959c3a7777a682a7c169e48849d3a18
Rogério Freitas Mateus <37122947+MikeHuggar@users.noreply.github.com>
2024-05-10 11:55:31 -0300

    Added collision functions
    No circle detection yet because i'm still figuring that out

--- scripts/geometry.js
+++ scripts/geometry.js
@@ -57,3 +57,45 @@ class Circle{
         context.stroke();
     }
 }
+
+//Collision functions, lacks circle detection at the moment
+
+function lineCollide(x1, y1, x2, y2, x3, y3, x4, y4) {
+    let uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3) * (x2-x1) - (x4-x3) * (y2-y1));
+
+    let uB = ((x2-x1) * (y1-y3) - (y2-y1) * (x1-x3)) / ((y4-y3) * (x2-x1) - (x4-x3) * (y2-y1));
+
+    return (uA >= 0 && uA <= 1 && uB >= 0 && uB <=1);
+}
+
+function polyLine(poly, x1, y1, x2, y2) {
+    var next = 0;
+    for (let current = 0; current < poly.hitbox.length; current++) {
+        next = current + 1;
+        if (next == poly.hitbox.length) next = 0;
+        var x3 = poly.hitbox[current].x;
+        var y3 = poly.hitbox[current].y;
+        var x4 = poly.hitbox[next].x;
+        var y4 = poly.hitbox[next].y;
+        var hit = lineCollide(x1, y1, x2, y2, x3, y3, x4, y4);
+        if (hit) {
+            return true;
+            }
+        }
+        return false;
+}
+
+function polyPolyCollide(poly1, poly2) {
+    var next = 0;
+    for (let current = 0; current < poly1.hitbox.length; current++) {
+        next = current + 1;
+        if (next == poly1.hitbox.length) next = 0;
+        var vc = poly1.hitbox[current];    // c for "current"
+        var vn = poly1.hitbox[next];       // n for "next"
+        var collision = polyLine(poly2, vc.x,vc.y,vn.x,vn.y);
+        if (collision) {
+            return true;
+            }
+        }
+        return false;
+}
